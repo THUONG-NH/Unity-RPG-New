6cd6bcf201a362506bf202272772c71cc4fe7773
Thuong-NH <thuong.nh@hotmail.com>
2025-06-04 17:44:06 +0700

    Idle and Movement animations

--- Assets/EntityState.cs
+++ Assets/EntityState.cs
@@ -3,19 +3,23 @@ using UnityEngine;
 public abstract class EntityState
 {
     protected StateMachine stateMachine;
-    protected string stateName;
+    protected string animBoolName;
     protected Player player;
 
-    public EntityState(StateMachine stateMachine, string stateName, Player player)
+    protected Animator anim;
+
+    public EntityState(StateMachine stateMachine, string animBoolName, Player player)
     {
         this.stateMachine = stateMachine;
-        this.stateName = stateName;
+        this.animBoolName = animBoolName;
         this.player = player;
+
+        anim = player.Anim;
     }
 
     public virtual void Enter()
     {
-
+        anim.SetBool(animBoolName, true);
     }
 
     public virtual void Update()
@@ -25,6 +29,6 @@ public abstract class EntityState
 
     public virtual void Exit()
     {
-
+        player.Anim.SetBool(animBoolName, false);
     }
 }
